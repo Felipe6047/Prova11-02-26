6f3bd57ecf85de6ececd67636b5aa906617c75e6
gustavo08k <ff063836421@gmail.com>
2026-02-11 17:22:48 -0300

    feat: adicionando html, css, script

---
+++ script.js
@@ -0,0 +1,14 @@
+const form = document.getElementById(formulariocontato);
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const campos = {
+     nome: document.getElementById('nome').value,
+     email: document.getElementById('email').value,
+    };
+ if (name <3, )
+ alert ("Nome:[nome]\nEmail:[email]")
+ form.reset();
+
+})
